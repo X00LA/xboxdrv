142a8af63957865c448466c541c0d94dabe3af9c
X00LA <X00LA@users.noreply.github.com>
2016-12-11 23:37:30 +0100

    Added XBox One Receiver and Controller to Device list.

--- src/xpad_device.cpp
+++ src/xpad_device.cpp
@@ -32,6 +32,8 @@ XPadDevice xpad_devices[] = {
   { GAMEPAD_XBOX,             0x045e, 0x0287, "Microsoft Xbox Controller S" },
   { GAMEPAD_XBOX,             0x045e, 0x0289, "Microsoft X-Box pad v2 (US)" },
   // { GAMEPAD_XBOX,          0x045e, 0x0288, "Microsoft Corp. Xbox Controller S Hub" },  memory card slot
+  { GAMEPAD_XBOX_WIRELESS,    0x045e, 0x02e6, "Xbox One Wireless Receiver" }, // official Wireless Receiver
+  { GAMEPAD_XBOX,             0x045e, 0x02dd, "Xbox One Wireless Controller" }, // official Wireless Controller
   { GAMEPAD_XBOX,             0x046d, 0xca84, "Logitech Xbox Cordless Controller" },
   { GAMEPAD_XBOX,             0x046d, 0xca88, "Logitech Compact Controller for Xbox" },
   { GAMEPAD_XBOX,             0x05fd, 0x1007, "Mad Catz Controller (unverified)" },
